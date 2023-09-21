9030b2586d9291d603c6685a339a10010c81f449
leote03 <107556461+leote03@users.noreply.github.com>
2023-09-21 10:15:33 +0100

    Initial version of main class.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Afonso Leote, n. 62580
+ * Demo for the Git lab on Software Engineering
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Animal git demo");
+    }
+}
